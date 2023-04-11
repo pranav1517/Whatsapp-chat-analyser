0db6280522c3da8423a4f4d7361416f5e5222173
Pranav Savani <103499639+pranav1517@users.noreply.github.com>
2023-04-11 23:22:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -124,5 +124,5 @@ if uploaded_file is not None:
 if st.sidebar.button('Show Links Data'):
     links
     
-csv = helpers.convert_df(userdf.drop('period',axis=1))
+csv = helper.convert_df(userdf.drop('period',axis=1))
 st.sidebar.download_button("Download Raw Data",csv,f"{selected_user}.csv","text/csv")
